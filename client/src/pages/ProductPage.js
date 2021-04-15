0c8718d886bfb47f42b694c64f40f4a689ef95cc
daniel simonson <dansimonson@mariposaweb.net>
2021-04-14 22:10:39 -0500

    loaded data with async axios call, added error and loading messages

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -51,7 +51,7 @@ function ProductPage(props) {
                     {product.countInStock > 0 ? (
                       <span className="success">In Stock</span>
                     ) : (
-                      <span className="error">Unavailable</span>
+                      <span className="danger">Unavailable</span>
                     )}
                   </div>
                 </div>
