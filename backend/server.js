29434eafeb9732b1d7c5286dc072ee1f5a87e50c
daniel simonson <dansimonson@mariposaweb.net>
2021-04-20 23:18:48 -0500

    added api for products

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from "express";
 //import mongoose from "mongoose";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
-import data from "./data.js";
+//import data from "./data.js";
 import userRouter from "./routers/userRouter.js";
+import productRouter from "./routers/productRouter.js";
 
 /**
  * mongodb+srv://dan123:dan123@cluster0.rty67.mongodb.net/store?retryWrites=true&w=majority
@@ -16,20 +17,21 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-app.get("/api/products/:id", (req, res) => {
+/*app.get("/api/products/:id", (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
     res.send(product);
   } else {
     res.status(404).send({ message: "Product Not Found" });
   }
-});
+});*/
 
-app.get("/api/products", (req, res) => {
+/*app.get("/api/products", (req, res) => {
   res.send(data.products);
-});
+});*/
 
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
