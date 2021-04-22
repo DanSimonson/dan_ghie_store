05b00f55fc2a431851f5305f77d4e62154f66f9d
daniel simonson <dansimonson@mariposaweb.net>
2021-04-22 01:47:45 -0500

    implemented backend authentication signin with email and password using a jsonwebtoken

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,25 @@
 import express from "express";
-//import mongoose from "mongoose";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
-//import data from "./data.js";
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
 
-/**
- * mongodb+srv://dan123:dan123@cluster0.rty67.mongodb.net/store?retryWrites=true&w=majority
- */
 dotenv.config();
 connectDB();
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-/*app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});*/
-
-/*app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});*/
-
+//routes
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
