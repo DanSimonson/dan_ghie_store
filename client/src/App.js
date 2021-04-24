5141c1363e9090385fb95096c68c34811714d068
daniel simonson <dansimonson@mariposaweb.net>
2021-04-24 12:40:20 -0500

    added placeorder page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import RegisterPage from "./pages/RegisterPage";
 import { signout } from "./actions/UserActions";
 import ShippingAddressPage from "./pages/ShippingAddressPage";
 import PaymentMethodPage from "./pages/PaymentMethodPage";
+import PlaceOrderPage from "./pages/PlaceOrderPage";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -57,6 +58,7 @@ function App() {
           <Route path="/register" component={RegisterPage}></Route>
           <Route path="/shipping" component={ShippingAddressPage}></Route>
           <Route path="/payment" component={PaymentMethodPage}></Route>
+          <Route path="/placeorder" component={PlaceOrderPage}></Route>
           <Route path="/" component={HomePage} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
