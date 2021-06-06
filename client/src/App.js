862463519fcbbf6c450762db6362134802101f40
daniel simonson <dansimonson@mariposaweb.net>
2021-06-06 00:35:45 -0500

    fixed bug where new product image did not show on page

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,12 @@ import CartPage from "./pages/CartPage";
 import SigninPage from "./pages/SigninPage";
 import RegisterPage from "./pages/RegisterPage";
 import { signout } from "./actions/UserActions";
+import AdminRoute from "./components/AdminRoute";
 import PrivateRoute from "./components/PrivateRoute";
 import ShippingAddressPage from "./pages/ShippingAddressPage";
 import PaymentMethodPage from "./pages/PaymentMethodPage";
 import PlaceOrderPage from "./pages/PlaceOrderPage";
+import ProductListPage from "./pages/ProductListPage";
 import OrderPage from "./pages/OrderPage";
 
 function App() {
@@ -61,11 +63,32 @@ function App() {
             ) : (
               <Link to="/signin">Sign In</Link>
             )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <Link to="#admin">
+                  Admin <i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link to="/productlist">Products</Link>
+                  </li>
+                  <li>
+                    <Link to="/orderlist">Orders</Link>
+                  </li>
+                  <li>
+                    <Link to="/userlist">Users</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </header>
         <main>
           <Route path="/cart/:id?" component={CartPage}></Route>
-          <Route path="/product/:id" component={ProductPage}></Route>
+          <Route path="/product/:id" component={ProductPage} exact></Route>
           <Route path="/signin" component={SigninPage}></Route>
           <Route path="/register" component={RegisterPage}></Route>
           <Route path="/shipping" component={ShippingAddressPage}></Route>
@@ -74,6 +97,10 @@ function App() {
           <Route path="/order/:id" component={OrderPage}></Route>
           <Route path="/orderHistory" component={OrderHistoryPage}></Route>
           <PrivateRoute path="/profile" component={ProfilePage}></PrivateRoute>
+          <AdminRoute
+            path="/productlist"
+            component={ProductListPage}
+          ></AdminRoute>
           <Route path="/" component={HomePage} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
