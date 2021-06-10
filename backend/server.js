c20268a1342451b7c5977873022951a93bd541e0
daniel simonson <dansimonson@mariposaweb.net>
2021-06-10 14:26:59 -0500

    serving files from build folder for deployment to heroku

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,10 @@ app.get("/api/config/paypal", (req, res) => {
 });
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/client/build/index.html"))
+);
 
 const port = process.env.PORT || 5000;
 
